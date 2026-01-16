3d94c30fbf47454ae29fb3b0b7cb57499194f503
Sakshi <sakshiwarkad6@gmail.com>
2026-01-16 21:29:44 +0530

    Create FirstProgram.py

---
+++ FirstProgram.py
@@ -0,0 +1,90 @@
+print("Hello World")
+print("Samiksha")
+
+print(23+67)
+
+print("my name is sam.", "23 is my age" )
+
+print("my name is sam.\n", "MIT is my college name.")
+
+#------------------------ Variables-------------------------------------------------------
+
+name = "Shraddha"   # string
+print(name)
+
+age = 23
+print(age)
+
+price = 23.45
+print(price)
+
+old = True
+a = None
+print(name ,age , price)
+
+print("My name is :" , name ,"\n and my age is:", age) 
+
+print( "price is:", price) 
+
+
+#---------Type of Data-------------------------------------------
+
+print(type(name))        # class string
+print(type(age))         # class int
+print(type(price))       # class float
+print(type(old))         # class bool
+print(type(a))           # class none Type
+
+#----------------String reppresentation---------------------------
+
+print("sk")           #Output -  sk
+print('sk')           # sk
+print('''sk''')       # sk
+
+#-------------------Keywords-----------------------------------------
+
+# keywords are reserved words in python
+
+
+#-------------------------Problems-----------------------------------
+
+# print sum of 2 numbersw
+
+num_1 = 3
+num_2 = 5
+print("sum :" , num_1 + num_2)
+
+
+name = (input("Enter your name:"))
+age = int(input("Enter your age:"))
+marks = int(input("Enter your marks:"))
+
+print("Your name is:", name)
+print("your age is:", age)
+print("your marks is:", marks)
+
+
+#WAP to input side of a square & print its area
+side = int(input("Enter square side:"))
+print("Area is = :", side*side )
+
+
+
+#WAP to input 2 floating point numbers & print their average
+num_1 = float(input("Enter the first float number:"))
+num_2 = float(input("Enter the second float number:"))
+
+avg = (num_1 + num_2)/2
+print("Average is:", avg)
+
+#WAP to input 2 int numbers, a and b.
+#Print True if a is greater than or equal to b. if not print False.
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+print(a>=b)
+
+print("Average is:", avg)
+
+
