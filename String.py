2b5dc3f2c9535d99a052125b7cf1290806033f2b
Sakshi <sakshiwarkad6@gmail.com>
2026-01-17 23:06:00 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,42 @@
+str = "Sakshi"
+str = 'Sakshi'
+str = """Sakshi"""
+
+print(str)
+
+#-------------string  practice------------------------
+
+str_1 = "Samiksha"
+str_2 = "Kulkarni"
+
+final_str = str_1 + str_2
+final_str = str_1 + " " + str_2
+print(final_str)
+
+#----------------Escape sequence character-----------------
+
+#\n - ----- for next line print
+# \t -------- for tab
+
+str_1 = "samiksha kulkarni. \n and her age is 23"
+print(str_1)
+
+str_2 = "samiksha kulkarni. \tand her age \tis 23"
+print(str_2)
+
+#-------------length of string---------------------------       
+
+str_1 = "HEY I AM SIDDHI"
+len = len(str_1)
+print(len)
+
+#------------------Indexing---------------------------------
+
+str_1 = "Apna"
+print(str_1[3])
+
+#----------error-------
+# str[0] = S
+#print(str_1)
+#------------------------
+
