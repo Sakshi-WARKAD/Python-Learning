d2567443cf103986fbdeb4ddc6bd48d8dc46ac88
Sakshi <sakshiwarkad6@gmail.com>
2026-01-18 19:48:37 +0530

    Create Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,370 @@
+"""age = 34
+
+if(age >= 18):
+    print("You can vote")
+else:
+    print("You can not vote")
+
+#-----------------------------------------------
+
+age = int(input("Enter your age here:"))
+
+if(age >= 18):
+    print("you can vote!")
+    print("Please proceed!")
+else:
+    print("You cannot vote!") 
+    
+
+
+#-----------------------------------------------------
+
+# Level 1 – Basic If
+#Take a number from the user and check whether it is positive, negative, or zero.
+
+Number = int(input("Enter a number:"))
+
+if(Number > 0):
+    print(" Positive number")
+
+elif( Number < 0 ):
+    print("Negative number")
+
+else:
+    print("zero number") 
+
+#-----------------------------------------------------------------------------------
+#Check whether a number is even or odd.
+
+Number = int(input("Enter a number:"))
+
+if( Number % 2 == 0):
+    print("Even Number")
+else:
+    print("Odd nummber")
+
+"""
+#-------------------------------------------------------------------------------------
+"""Take age as input and print:
+
+"Child" if age < 13
+
+"Teen" if 13–19
+
+"Adult" if 20–59
+
+"Senior" if 60+
+
+"""
+"""
+Age = int(input("Enter your age:"))
+    
+if(Age < 13):
+    print("Child")
+
+if(Age >= 13 and Age <= 19):
+    print("Teen")
+
+if(Age >= 20 and Age <= 59):
+    print("Adult")
+
+if ( Age > 60):
+    print("Senior")
+
+#----------------------------------------------------------------------------------
+#Check whether a given number is divisible by both 3 and 5.
+
+Number = int(input("Enter a number:"))
+
+if(Number % 3 ==0 and Number % 5 == 0):
+    print("It is divisible by both 3 & 5")
+else:
+    print("It is not divisible by both 3 and 5")
+
+#------------------------------------------------------------------------------
+#Level 2 – If-Else Logic
+
+#Find the largest of two numbers.
+
+Number_1 = int(input("Enter your 1st number:"))
+Number_2 = int(input("Enter your 2nd number:"))
+
+if(Number_1 > Number_2):
+   print("This is the largest number", Number_1)
+else:
+   print("This is the largest number", Number_2)
+
+"""
+#-----------------------------------------------------------------------
+#Ask the user for a password:
+
+"""
+If correct → "Access Granted"
+
+Else → "Wrong Password"
+
+"""
+#Password = int(input("Enter your password"))
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
