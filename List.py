6da1948d2c6a8e892b96d3b2b26677cfa52d9e61
Sakshi <sakshiwarkad6@gmail.com>
2026-01-19 21:41:37 +0530

    Added list

---
+++ List.py
@@ -0,0 +1,21 @@
+#--------------------------List in python-----------------------------------------------------------------------------------
+# A built in data type that stores set of values. it can store elements of different type(Integer, float, string etc.)
+# String - - immutable in python (Can't change)
+# List -- mutable in python (change) 
+
+list = ["Krishna", 12.45 , "Samiksha", 45, "Kamyar", "Haniya"]
+print(list)
+
+list[2] = "Roshi"
+print(list)
+
+"""str = "Hello"
+str[0] = "k"
+                                              #It will through an error bcz strings are immutable in python.
+print(str[0])
+"""
+
+list[0] = "Saras"
+ 
+print(list[0])
+print(list)
